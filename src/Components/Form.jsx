454b9411b051bdc7e172590d5d0866aeadfae664
RahuBirla <birlarahul340@gmail.com>
2023-02-15 11:39:32 +0530

    Form is updated

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,22 +3,46 @@ import { useState } from 'react';
 
 const Form = () => {
 
-    const [inputData, setInputData] = useState("pramod");
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        number: "",
+        textarea: "",
+        sel:"Kia",
+    });
+
 
     const handleChange = (e) => {
-        let x = e.target.value
-        setInputData(x);
+        let val = e.target.value;
+        let name = e.target.name;
+        setInput({ ...input, [name]: val })
     }
 
-    console.log(inputData);
+
+    // let obj = { name: "Rahul" };
+    // let obj2 = { ...obj, name: "pramod" };
+
 
     return (
         <>
             <form>
-                <input type="text" value={inputData} name="name" id="name" onChange={handleChange}/>
+                <input type="text" value={input.name} name="name" id="name" onChange={handleChange} />
+                <input type="email" value={input.email} name="email" id="email" onChange={handleChange} />
+                <input type="text" value={input.number} name="number" id="number" onChange={handleChange} />
+
+                {/* textarea */}
+                <textarea name="textarea" id="textarea" cols="30" defaultValue={input.textarea} rows="10" onChange={handleChange} />
+
+                {/* selected */}
+                <select name="sel" id="sel" value={input.val} onChange={handleChange} >
+                    <option value="Honda">Honda</option>
+                    <option value="BMW" selected >BMW</option>
+                    <option value="Kia">Kia</option>
+                </select>
+
             </form>
         </>
     )
 }
 
-export default Form
+export default Form;
