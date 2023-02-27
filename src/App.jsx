59697ce6361d2fdb967f1b3d1670eef7a1ef9438
RahuBirla <birlarahul340@gmail.com>
2023-02-27 12:50:31 +0530

    useEffect Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import React from "react";
-import "./App.css";
-import Form from "./Components/Form";
-import IncreDecre from "./Components/IncreDecre";
-import Navbar from "./Components/Navbar";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import './App.css';
 
 
 const App = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/About" element={<About />} />
-                    <Route path="/IncreDecre" element={<IncreDecre />} />
-                    <Route path="/Form" element={<Form />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
+    // useEffect
+
+    const [count, setCount] = useState(0);
+    const [multi, setMulti] = useState(0);
+
+    const handleClick = () => {
+        setCount((c) => c + 1);
+    }
+
+    // useEffect(() => {
+    //     setMulti(count * 2);
+    // }, [count])
+
+    let timer;
+
+    useEffect(() => {
+       timer =  setTimeout(() => {
+            setCount((c) => c + 1);
+       }, 1000)
+        
+    })
+
+    const StopTimmer = () => {
+        clearTimeout(timer);
+    }
+
+
+    return <>
+        <div className="container">
+            <div className="movieCard">
+                <h1>{`count :${count}`}</h1>
+                <h1>{`multi :${multi}`}</h1>
+                <button onClick={handleClick} >Increment</button>
+                <button onClick={StopTimmer} >Stop</button>
+            </div>
+        </div>
+    </>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
         <App />
